d87cdfcbb4524993093e28de4371c9c4b787da71
Timothy Miko <timothymiko@gmail.com>
2016-03-19 17:59:28 -0400

    Add view controller for OpenCV camera.

---
+++ SetAI/OpenCVCameraViewController.swift
@@ -0,0 +1,19 @@
+//
+//  OpenCVCameraViewController.swift
+//  SetAI
+//
+//  Created by Timothy Miko on 3/19/16.
+//  Copyright © 2016 Kyle Haptonstall. All rights reserved.
+//
+
+import UIKit
+
+class OpenCVCameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- SetAI/OpenCVWrapper.h
+++ SetAI/OpenCVWrapper.h
@@ -7,7 +7,9 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <opencv2/highgui/cap_ios.h>
 
-@interface OpenCVWrapper : NSObject
+@interface OpenCVWrapper : NSObject <CvVideoCameraDelegate>
+
 
 @end
