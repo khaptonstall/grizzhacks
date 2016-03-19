fab572f7de37d9ea344a313a7f1d1073c1c93b54
Sahil Ambardekar <sahil.ambardekar@yahoo.com>
2016-03-19 14:30:16 -0400

    basic Camera/Scanner

--- SetAI/ViewController.swift
+++ SetAI/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import MobileCoreServices
 
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var containerView: UIView!
+    var picker = UIImagePickerController()
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
 
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            picker.sourceType = UIImagePickerControllerSourceType.Camera
+            picker.mediaTypes = [kUTTypeImage as String]
+        }
+        
+        picker.delegate = self
+        picker.allowsEditing = false
+        picker.showsCameraControls = false
+        
+        self.addChildViewController(picker)
+        containerView.addSubview(picker.view)
+        containerView.layer.masksToBounds = true
+        
     }
-
-
+    
+    @IBAction func scan(sender: AnyObject) {
+        
+        picker.takePicture()
+    }
+    
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            
+            print("Got image!")
+        }
+    }
 }
 
