d7bc38dd977e84cf9c343caeaa24c7f890be7f11
Timothy Miko <timothymiko@gmail.com>
2016-03-19 15:08:52 -0400

    Add designs for iOS app. Add .gitignore.

--- Podfile
+++ Podfile
@@ -6,4 +6,5 @@
 target 'SetAI' do
 pod 'p2.OAuth2'
 pod 'Alamofire', '~> 3.0'
+pod 'OpenCV'
 end
---
+++ Pods/Target Support Files/Pods-SetAI/Pods-SetAI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SetAI : NSObject
+@end
+@implementation PodsDummy_Pods_SetAI
+@end
